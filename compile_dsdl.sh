571c3e780f58b279067d7c5649fea17a8c6399d3
David Lenfesty <lenfesty@ualberta.ca>
2019-01-16 15:09:55 -0700

    Added some output to compile script

--- compile_dsdl.sh
+++ compile_dsdl.sh
@@ -12,5 +12,6 @@ LIBUAVCAN_OUTDIR="libuavcan_dsdlc_generated"
 DSDL_INPUT="spear/ uavcan_dsdl/uavcan/"
 
 # Actually run the commands
+echo "Compiling DSDL..."
 ${LIBCANARD_CMD} --outdir ${LIBCANARD_OUTDIR} ${DSDL_INPUT} && \
 ${LIBUAVCAN_CMD} --outdir ${LIBUAVCAN_OUTDIR} ${DSDL_INPUT}
